18029a7adad7ae483b219c2d901313e96b6b08d0
Kairui Song <kasong@tencent.com>
2026-01-26 01:57:35 +0800

    mm, swap: no need to clear the shadow explicitly
    Since we no longer bypass the swap cache, every swap-in will clear the
    swap shadow by inserting the folio into the swap table. The only place
    we may seem to need to free the swap shadow is when the swap slots are
    freed directly without a folio (swap_put_entries_direct). But with the
    swap table, that is not needed either. Freeing a slot in the swap table
    will set the table entry to NULL, which erases the shadow just fine.
    
    So just delete all explicit shadow clearing, it's no longer needed.
    Also, rearrange the freeing.
    
    Signed-off-by: Kairui Song <kasong@tencent.com>
    Tested-by: syzbot@syzkaller.appspotmail.com

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -1291,7 +1291,6 @@ static void swap_range_alloc(struct swap_info_struct *si,
 static void swap_range_free(struct swap_info_struct *si, unsigned long offset,
 			    unsigned int nr_entries)
 {
-	unsigned long begin = offset;
 	unsigned long end = offset + nr_entries - 1;
 	void (*swap_slot_free_notify)(struct block_device *, unsigned long);
 	unsigned int i;
@@ -1316,7 +1315,6 @@ static void swap_range_free(struct swap_info_struct *si, unsigned long offset,
 			swap_slot_free_notify(si->bdev, offset);
 		offset++;
 	}
-	__swap_cache_clear_shadow(swp_entry(si->type, begin), nr_entries);
 
 	/*
 	 * Make sure that try_to_unuse() observes si->inuse_pages reaching 0
